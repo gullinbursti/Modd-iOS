d0f56346d194d79d8879b2bd9836eaba67b2359e
Matt Holcombe <matt@builtinmenlo.com>
2015-11-18 11:25:09 -0800

    Initial Commit

---
+++ Modd/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Modd
+//
+//  Created by Matthew Holcombe on 11/18/15.
+//  Copyright © 2015 Built In Menlo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
